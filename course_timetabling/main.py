352f39c0bb7638d92e53ab9da404146758c95500
Gabrielly de Andrade <gabriellydandrade@gmail.com>
2024-09-15 15:06:33 -0300

    refactor: removing not used files

--- course_timetabling/main.py
+++ course_timetabling/main.py
@@ -10,12 +10,8 @@ P = input.professores
 D = input.disciplinas
 DISCIPLINA_DIAS = input.disciplinas_dias
 DISCILINA_HORARIOS = input.disciplinas_horarios
-# S = input
-# S = input.dia_semana
-# H = input.horarios
 PROFESSOR_APTO = input.professores_area_conhecimento
 AREAS_CONHECIMENTO = input.areas_conhecimento
-# HORARIOS = input.horarios
 
 def get_carga_horaria(disciplina):
     # Retorna um horário se todos forem iguais
@@ -66,7 +62,6 @@ for p in P:
         X[p][d] = {}
 
         CH = get_carga_horaria(d)
-        # for CH in D[d][1]: # pega carga horária
         f[p][d][CH[0]] = {}
         f[p][d][CH[0]][CH[1]] = {}
 
@@ -107,41 +102,9 @@ def get_disciplinas_a_partir_de_um_dia(disciplinas: dict, dia:str):
     result = set(keys)
     return result
 
-# def get_professores_a_partir_de_uma_disciplina(professores: dict, disciplina:str):
-#     keys = []
-#     for professor,detalhe in professores.items():
-#         for carga_horaria in detalhe[1]:
-#             if carga_horaria[0] == disciplina:
-#                 keys.append(professor)
-#     return set(keys)
-
-# Fixado uma disciplina de uma turma, só poderemos ter um professor alocado
-#FIXME
-# Solution count 1: 2 
-
-# Optimal solution found (tolerance 1.00e-04)
-# Best objective 2.000000000000e+00, best bound 2.000000000000e+00, gap 0.0000%
-# Adriana Vivacqua_ICP489_SEG_8:00-10:00 1
-# Adriana Vivacqua_ICP489_QUA_8:00-10:00 0
-# Adriana Vivacqua_ICP102_QUA_8:00-10:00 0
-# Adriana Vivacqua_ICP102_SEX_8:00-10:00 1
-# Daniel Sadoc_ICP489_SEG_8:00-10:00 0
-# Daniel Sadoc_ICP489_QUA_8:00-10:00 0
-# Daniel Sadoc_ICP102_QUA_8:00-10:00 0
-# Daniel Sadoc_ICP102_SEX_8:00-10:00 0
-# Obj: 2
 for d in D.keys():
-    # for s in S:
-    #     for h in H:
     CH = get_carga_horaria(d)
-    # for CH in D[d][1]:
-        # A = get_professores_a_partir_de_uma_disciplina(D, CH[0])
-        # C = A.intersection(B)
     m.addConstr(gp.quicksum([X[p][d][CH[0]][CH[1]] for p in P]) == 1)
-    # for CH in D[d][1]: # pega carga horária
-    # dia_fixo = D[d][1][0][0]
-    # hora_fixa = D[d][1][0][1]
-    # m.addConstr(gp.quicksum([X[p][d][dia_fixo][hora_fixa] for p in P]) == 1)
 
 # Fixado um professor, esse só pode dar aula em um único dia e horário
 
@@ -157,18 +120,6 @@ for p in P:
 
         dias = DISCIPLINA_DIAS[i].split(",")
         m.addConstr(gp.quicksum([X[p][d][get_carga_horaria(d)[0]][get_carga_horaria(d)[1]] for d in C]) <= 1)
-        # m.addConstr(gp.quicksum([X[p][d][DISCIPLINA_DIAS[i]][DISCILINA_HORARIOS[i]] for d in C for dia in dias if dia in DISCIPLINA_DIAS[i]]) <= 1)
-    # for s in DISCIPLINA_DIAS: #FIXME: adicionar aqui possibilidades de combinacoes de dia da semana
-    #     print(DISCIPLINA_DIAS)
-    #     A = get_disciplinas_a_partir_de_um_dia(D, s)
-    #     for h in DISCILINA_HORARIOS: #FIXME: adicionar aqui possibilidades de combinacoes de hora
-    #         print(DISCILINA_HORARIOS)
-    #         B = get_disciplinas_a_partir_de_um_horario(D, h)
-    #         C = A.intersection(B)
-    #         print(s,h)
-    #         print(A,B,C)
-    #         # m.addConstr(gp.quicksum([X[p][d][s][h] for d in C if s in X[p][d] and h in X[p][d][s]]) <= 1)
-    #         m.addConstr(gp.quicksum([X[p][d][s][h] for d in C]) <= 1)
 
 # TODO: não deixar que um professor sem ser de uma área ministre uma disciplina. Ex.: Daniel Sadoc_ICP370_TER,QUI_8:00-10:00 1
 # TODO: adicionar professor dummy para que ele seja alocado caso não tenha professor disponível
