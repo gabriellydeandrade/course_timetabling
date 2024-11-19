9185c771cc857e078fed92c1ee34a14c573dad71
Gabrielly de Andrade <gabriellydandrade@gmail.com>
2024-11-18 21:41:53 -0300

    refactor: improving visualization

--- course_timetabling/results/generate_timetabling.py
+++ course_timetabling/results/generate_timetabling.py
@@ -18,29 +18,57 @@ df = pd.read_csv(
     ],
 )
 
+df_pcb = pd.read_csv(
+    "course_timetabling/results/pcb_professors.csv",
+    delimiter=";",
+    names=[
+        "professor",
+        "credits_below"
+    ],
+)
+
 st.set_page_config(
     layout="wide",
 )
 
 st.title("Course Timetabling - Resultados")
-st.header("Alocação final de professores em disciplinas", divider="gray")
+st.header("Alocação final de professores em disciplinas", divider="rainbow")
 
 with st.container(border=True) as general:
 
-    general_without_dummy = df.loc[df["professor"] != "DUMMY"]
-
-    courses_svc = df.loc[df["course_type"] == "SVC"]
-    courses_opt = df.loc[df["course_type"] == "OPT"]
-    courses_obg = df.loc[df["course_type"] == "OBG"]
-
     st.subheader("Visão geral")
 
-    st.metric(label="Qtd de disciplinas obrigatórias", value=len(courses_obg))
-    st.metric(label="Qtd de disciplinas de serviço", value=len(courses_svc))
-    st.metric(label="Qtd de disciplinas eletivas", value=len(courses_opt))
+    general_without_dummy = df.loc[df["professor"] != "DUMMY"]
 
-    st.data_editor(
-        general_without_dummy,
+    all_professors = general_without_dummy["professor"].unique().tolist()
+    all_professors.insert(0, "Todos")
+
+    options = st.multiselect(
+        "Seleciona um docente",
+        all_professors,
+        ["Todos"]
+    )
+
+    if "Todos" in options:
+        selected_professors = general_without_dummy["professor"].unique()
+    else:
+        selected_professors = options
+
+    df_filter = general_without_dummy[general_without_dummy["professor"].isin(selected_professors)]
+
+    courses_svc = df_filter.loc[df_filter["course_type"] == "SVC"]
+    courses_opt = df_filter.loc[df_filter["course_type"] == "OPT"]
+    courses_obg = df_filter.loc[df_filter["course_type"] == "OBG"]
+
+
+    col1, col2, col3 = st.columns(3)
+
+    col1.metric(label="Qtd de disciplinas obrigatórias", value=len(courses_obg))
+    col2.metric(label="Qtd de disciplinas de serviço", value=len(courses_svc))
+    col3.metric(label="Qtd de disciplinas eletivas", value=len(courses_opt))
+
+    st.dataframe(
+        df_filter,
         column_config={
             "responsable_institute": "Instituto responsável",
             "professor": "Docente",
@@ -58,6 +86,28 @@ with st.container(border=True) as general:
             ),
             "capacity": "Vagas",
             "classroom_type": "Tipo de sala",
+            "course_type": "Tipo de disciplina",
+            "term": "Período",
+        },
+        hide_index=True,
+        use_container_width=False,
+    )
+
+with st.container(border=True) as pcb:
+    st.subheader("Visão professores abaixo da carga horária mínima")
+    st.text("Professores que estão abaixo da carga horária mínima de 8 créditos.")
+
+    st.metric(label="Qtd de professores", value=len(df_pcb), help=f"Existem {len(df_pcb)} professores no PCB.")
+
+    df_pcb["credits_below_percent"] =  ((df_pcb["credits_below"])/8)*100 
+    df_pcb = df_pcb.sort_values(by="credits_below_percent", ascending=False)
+
+    st.dataframe(
+        df_pcb,
+        column_config={
+            "professor": "Docente",
+            "credits_below": "Créditos abaixo",
+            "credits_below_percent": st.column_config.ProgressColumn(label="Percentual abaixo do ideal", min_value=0, max_value=100, format="%f%%"),
         },
         hide_index=True,
         use_container_width=False,
@@ -71,7 +121,7 @@ with st.container(border=True) as dummy:
 
     st.metric(label="Qtd de DUMMY", value=len(dummy), help=f"Existem {len(dummy)} disciplinas sem professores.")
 
-    st.data_editor(
+    st.dataframe(
         dummy,
         column_config={
             "responsable_institute": "Instituto responsável",
@@ -90,6 +140,8 @@ with st.container(border=True) as dummy:
             ),
             "capacity": "Vagas",
             "classroom_type": "Tipo de sala",
+            "course_type": "Tipo de disciplina",
+            "term": "Período",
         },
         hide_index=True,
         use_container_width=False,
